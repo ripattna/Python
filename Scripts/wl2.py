78350e57998fb1c36cc50799bf9b1c2203e75c24
Rissan Pattnaik <rissan@MacBook.local>
2022-01-01 22:10:54 +0530

    Code Change

--- Scripts/wl2.py
+++ Scripts/wl2.py
@@ -1,16 +1,16 @@
 i=1
 while i<=10:
-    print i,
+    print (i),
     i+=1
 print
-print i
+print(i)
 print 
 i=1
 while i<=10:
     if i%2==0:
         i+=1
         continue
-    print i,
+    print(i),
     i+=1
 print
-print i
+print(i)
